ee7bd4a8aa67aa4db0e98acb83e93d0cefe2252e
Uisshu <phngvnc@outlook.com>
2022-04-06 00:04:39 +0200

    Touch Pin LED finished

--- Touch_Pin_LED/src/main.cpp
+++ Touch_Pin_LED/src/main.cpp
@@ -1,9 +1,35 @@
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
+// set pin numbers
+#define touchPin T0 //GIPO 4
+const int ledPin = 16;
+
+// set the threshold
+const int threshold = 20;
+// variable for storing the touch pin value
+int touchValue;
+
+void setup(){
+  Serial.begin(115200);
+  delay(1000); //time to bring up serial monitor
+  // initialize the LED pin as an output
+  pinMode (ledPin, OUTPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop(){
+  // read the state of the pushbutton value
+  touchValue = touchRead(touchPin);
+  Serial.print(touchValue);
+  // check if the touch pin value is below the threshold
+  if(touchValue < threshold){
+    // if it is, turn the led on
+    digitalWrite(ledPin, HIGH);
+    Serial.println(" - LED on");
+  }
+  else{
+    // if not, turn off
+    digitalWrite(ledPin, LOW);
+    Serial.println(" - LED off");
+  }
+  delay(500);
 }
